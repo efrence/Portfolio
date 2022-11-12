2b9b8e889abb9f9e9e1b20a7c72b8b02f665725e
Efren Carbajal <efren_ce@hotmail.com>
2022-11-12 11:17:43 -0700

    Add quasar layout

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,4 +1,22 @@
 <template>
-  <span></span>
-  <Content />
+  <q-layout view="lHh lpr lFf" container>
+    <q-page-container>
+      <q-page class="flex flex-col">
+        <div class="absolute w-full h-full -z-10 bg-theme-gradient bg-size-400% anim-gradient-15-linear-infinite"></div>
+          <div class="p-0 sm:p-4 anim-gradient-30-ease-infinite bg-size-400% flex-1">
+            <div class="h-page-full sm:h-page max-h-page-full sm:max-h-page">
+              <Content />
+            </div>
+          </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
+
+<style>
+html,
+body,
+#app {
+  height: 100vh;
+}
+</style>
